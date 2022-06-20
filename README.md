45b497bfd969be53e1792578523a111560e9b34d
Lawrence Leppert <lxleppert@gmail.com>
2022-06-19 21:07:16 -0600

    First functional commit

--- README.md
+++ README.md
@@ -1,7 +1,63 @@
 # MakeChangeProject
 
-## Description 
+## Description
+
+Program to calculate the amount of change based on an item's cost and the amount
+tendered by the customer.
+
+Amounts and types of change are displayed based on input.
+
+### Usage:
+User inputs item price and cash tender by customer when prompted
+
+#### User Inputs
+- Item price amount
+- Amount tendered by customer
+
+##### User Input Constraints
+- Item price must be greater than $00.01
+- Item price must be less than $1,000,000.00
+- Amount tendered must be greater than or equal to item price
+
+#### Display Outputs
+- Item Amount
+- Tendered Amount
+- Change Amount
+- Change denominations and amounts
+
+###### Output Example
+
+```formatted output
+Enter Item Price: 
+10.01
+Enter Amount Tendered by Customer: 
+88.68
+
+Item Amount     : $      10.01
+Tendered Amount : $      88.68
+                  ------------
+Change Amount   : $      78.67
+
+*-----------------------------*
+*  Give the following change  *
+*-----------------------------*
+| Bills:                      |
+|          $ 20 :   3         |
+|          $ 10 :   1         |
+|           $ 5 :   1         |
+|           $ 1 :   3         |
+| Coins:                      |
+|      Quarters :   2         |
+|          Dime :   1         |
+|        Nickel :   1         |
+|       Pennies :   2         |
+*-----------------------------*
+```
 
 ## Technologies Used
+- Java 8
+- Eclipse on MacOS
 
 ## What I Learned
+- Eclipse refactoring
+- Floating point variable precision is difficult with financial numbers
--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -4,6 +4,104 @@ public class MakeChange {
 
 	public static void main(String[] args) {
 		
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+
+		double itemAmount;
+		double tenderedAmount;
+		double changeAmount;
+		
+		int dollarAmount;
+		int centAmount;
+
+		System.out.println("Enter Item Price: ");
+		itemAmount = sc.nextDouble();
+
+		if (itemAmount < .01) {
+			System.err.println("Invalid Item Price");
+			
+		} else if (itemAmount >= 100000) {
+			System.err.println("No Item Prices That High");
+			
+		} else {
+			System.out.println("Enter Amount Tendered by Customer: ");
+			tenderedAmount = sc.nextDouble();
+
+			if (tenderedAmount < itemAmount) {
+				System.err.println("Amount Tendered by Customer Lower than Item Price");
+				
+			} else {
+				System.out.printf("\nItem Amount     : $  %9.2f%n", itemAmount);
+				System.out.printf("Tendered Amount : $  %9.2f%n", tenderedAmount);
+
+				changeAmount = tenderedAmount - itemAmount;
+				
+				System.out.println("                  ------------");
+				System.out.printf("Change Amount   : $  %9.2f%n", changeAmount);
+
+				dollarAmount = (int) changeAmount;
+				
+				if (changeAmount == 0) {
+					System.out.println("\n*-----------------------------*");
+					System.out.println("*     Exact Amount Given      *");
+					System.out.println("*     No Change Necessary     *");
+					System.out.println("*-----------------------------*");
+				}
+
+				if (dollarAmount == 0) {
+					centAmount = (int) ((changeAmount + .005) * 100);
+					
+				} else {
+					centAmount = (int) (((changeAmount + .005) % dollarAmount) * 100);
+				}
+				
+				if ((dollarAmount != 0) || (centAmount != 0)) {
+					System.out.println("\n*-----------------------------*");
+					System.out.println("*  Give the following change  *");
+					System.out.println("*-----------------------------*");
+
+					if (dollarAmount != 0) {
+						System.out.println("| Bills:                      |");
+						dollarAmount -= calcChange(dollarAmount, 20, "$ 20");
+						dollarAmount -= calcChange(dollarAmount, 10, "$ 10");
+						dollarAmount -= calcChange(dollarAmount, 5, "$ 5");
+						dollarAmount -= calcChange(dollarAmount, 1, "$ 1");
+					}
+
+					if (centAmount != 0) {
+						System.out.println("| Coins:                      |");
+						centAmount -= calcChange(centAmount, 25, "Quarter");
+						centAmount -= calcChange(centAmount, 10, "Dime");
+						centAmount -= calcChange(centAmount, 5, "Nickel");
+						centAmount -= calcChange(centAmount, 1, "Penny");
+					}
+					System.out.println("*-----------------------------*");
+				}
+			}
+		}
+		sc.close();
 	}
 
+	public static int calcChange(int amount, int denomination, String changeType) {
+
+		int denominationCount = 0;
+
+		if (amount >= denomination) {
+			
+			denominationCount = amount / denomination;
+			
+			if (denominationCount > 1) {
+				if (changeType.equals("Quarter")) {
+					changeType = "Quarters";
+					
+				} else if (changeType.equals("Dime")) {
+					changeType = "Dimes";
+					
+				} else if (changeType.equals("Penny")) {
+					changeType = "Pennies";
+				}
+			}
+			System.out.printf("| %13s :   %d         |\n", changeType, denominationCount);
+		}
+		return denominationCount * denomination;
+	}
 }
